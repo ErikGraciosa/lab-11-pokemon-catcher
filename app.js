9d3495be4c05c80183a83a6a24537f0e5ed6ae9f
ErikGraciosa <erikgraciosa@gmail.com>
2020-10-12 19:02:14 -0700

    function for three random numbers testing good

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
+import { generateThreeUniqueNumbers } from './functions.js';
+
+generateThreeUniqueNumbers();
 // import functions and grab DOM elements
+//grab the images with radio buttons, the uls for updating state, the buttons,
+
 
 // initialize state
+//init the pokemon list, init the number of pokemon caught, any kinf of init for formatting of the pictures?
+
+
 
 // set event listeners to update state and DOM
+//Event listener for change of state on the radio button
+
+
+
+
---
+++ archivefunction.js
@@ -0,0 +1,31 @@
+//Create a function that makes three unique random numbers
+
+
+function randomNumber() {
+    const lengthOfPokedex = 15;
+    return Math.floor(Math.random() * lengthOfPokedex);
+}
+
+export function generateThreeUniqueNumbersArchive() {
+    let array = [randomNumber(), randomNumber(), randomNumber()];
+    let i = 0;
+
+    if (array[0] === array[1] || array[1] === array[2] || array[0] === array[2]) {
+        console.log('this is before the recursive call ' + array);
+        i++;
+        array.splice(0, 3, generateThreeUniqueNumbersArchive());
+    } else {
+        return array;
+    }
+    
+    console.log('this is after the recursive call ' + array);
+    if (i === 0) {
+        return array;
+    } else {
+        const weirdObject = [array[0][0], array[0][1], array[0][2]];
+        console.log('the weird object ' + weirdObject);
+        return weirdObject;
+    }
+    
+}
+
